0cb6f8a0c93542add883acb674633c7cc4cca3b2
Mgamboa <marianellag400@gmail.com>
2022-09-06 09:06:24 -0500

    moving files, error on page display

---
+++ jscript.js
@@ -0,0 +1,6 @@
+// questions:
+// When data is stored in a single variable, it's called a(n): array  can choose: boolean-list-object
+// HTML gives structure to the page, CSS adds style to the page, while JavaScript gives... interaction ... to the page, Github,Heading, Class
+//When writing code, all should end with... ;... /,?, ^
+//A variable declared in global scope is available to ...all... function(s) , some , none, one
+//DOM stands for... Document Object Model... Doughnuts on Monday, Don't Open Meeting, Drove On Mars
