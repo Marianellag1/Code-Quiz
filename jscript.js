242f16285ac4c123144c5293987b64b1fd6b445f
Mgamboa <marianellag400@gmail.com>
2022-09-07 17:16:13 -0500

    got one question to showgit add .

--- jscript.js
+++ jscript.js
@@ -1,12 +1,16 @@
 var startButton = document.querySelector("#start-button");
-var scoreResult = document.querySelector("#score-results");
 var quizTimer = document.querySelector(".quiz-timer");
-var rightAnswer;
+var questions = document.querySelector("#questions");
+var endScreen = document.querySelector("#end-screen");
+var choices = document.querySelector("#choices");
+var questionTitle = document.querySelector("#question-title");
+var startScreen = document.querySelector("#start-screen");
 var secondsLeft = 50;
-var count = localStorage.getItem("count");
-
-var answerChoices = document.getElementById("answer-choices");
 var questionIndex = 0;
+// var scoreResult = document.querySelector("#score-results");
+// var rightAnswer;
+// var count = localStorage.getItem("count");
+// var answerChoices = document.getElementById("answer-choices");
 
 startButton.addEventListener("click", function () {
     var timerInterval = setInterval(function () {
@@ -14,52 +18,65 @@ startButton.addEventListener("click", function () {
         quizTimer.textContent = "Time: " + secondsLeft + " second(s) left";
         if (secondsLeft == 0) {
             clearInterval(timerInterval);
+            endScreen.removeAttribute("class");
         }
     }, 1000);
-
+    startScreen.style.display = "none";
+    // questions.style.display = "block";
+    startQuiz();
+    
 });
 
- function startQuiz() {
- var queryQuiz = quizQuest[i];
-//  questTitle.textContent = queryQuiz.text;
- answerChoices.innerHTML = "";
 
-    for (var i = 0; i < queryQuiz.length; i++) {
-    var choiceButton = document.createElement('button');
+// function startQuiz(){
+//     questions.removeAttribute("class");
+//     getQuestions();
+// }
 
-    choiceButton.setAttribute("class", "btn");
-    choiceButton.textContent = queryQuiz.choices[i];
-    answerChoices.appendChild(choiceButton);
+function startQuiz() {
+    let currentQuestion = questions[questionIndex];
+    questionTitle.textContent = currentQuestion.title;
+    choices.innerHTML = "";
+
+    // for (let i = 0; i < currentQuestion.choices.length; i++) {
+    //     var choice = currentQuestion.choices[i];
+    //     var choiceButton = document.createElement('button');
+    //     choiceButton.setAttribute("class", "choice-btn");
+    //     choiceButton.setAttribute("value", choice - button);
+    //     choiceButton.textContent = i + 1 + "." + choice;
+    //     questionChoices.appendChild(choiceButton);
+
+    // }
+}
+
+document.querySelector("#questions").addEventListener("click", function(e){
+    console.log(e.target);
+    if(questionIndex === questions.length - 1){
+        alert("last question")
+    }else{
+        questionIndex++;
+        getQuestions();
     }
-};
-var quizQuest = [
-    {
-        title: "When data is stored in a single variable, it's called a(n):",
-        choices: ["Boolean", "List", "Object", "Array"],
-        answer: "Array",
-    },
-    {
-        title: "HTML gives structure to the page, CSS adds style to the page, while JavaScript gives the user... to the page.",
-        choices: ["Github", "Interaction", "Heading", "Class"],
-        answer: "Interaction",
-    },
-    {
-        title: "When writing code, all should end with...",
-        choices: ["?", "~", ";", "^"],
-        answer: ";",
-    },
-    {
-        title: "A variable declared in global scope is available to... function(s).",
-        choices: ["Some", "All", "None", "One"],
-        answer: "All",
-    },
+})
 
-    {
-        title: "D.O.M. stands for...",
-        choices: ["Document Object Model", "Doughnuts on Monday", "Don't Open Meeting", " Drove On Mars"],
-        answer: "Document Object Model",
-    },
-]
+function questionAnswerSelected(){
+    if(choiceButton.value !== answer){
+        var wrongAnswer = "Wrong Answer";
+        quizTimer = quizTimer - 10;
+        wrongAnswer.textContent = "Wrong Answer";
+        clearInterval(currentQuestion);
+        choice =questionIndex + 1;
+    }else {
+        var correctAnswer = "Correct Answer";
+        correctAnswer.textContent = "Correct Answer";
+        localStorage.setItem;
+
+    }
+
+
+}
+// };
+
 // quizQuest.addEventListener("click");
 
 // var questTitle = document.querySelector(".btn").addEventListener("click", function () {
---
+++ questions.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+        title: "When data is stored in a single variable, it's called a(n):",
+        choices: ["Boolean", "List", "Object", "Array"],
+        answer: "Array",
+    },
+]
+    // {
+    //     title: "HTML gives structure to the page, CSS adds style to the page, while JavaScript gives the user... to the page.",
+    //     choices: ["Github", "Interaction", "Heading", "Class"],
+    //     answer: "Interaction",
+    // },
+    // {
+    //     title: "When writing code, all should end with...",
+    //     choices: ["?", "~", ";", "^"],
+    //     answer: ";",
+    // },
+    // {
+    //     title: "A variable declared in global scope is available to... function(s).",
+    //     choices: ["Some", "All", "None", "One"],
+    //     answer: "All",
+    // },
+
+    // {
+    //     title: "D.O.M. stands for...",
+    //     choices: ["Document Object Model", "Doughnuts on Monday", "Don't Open Meeting", " Drove On Mars"],
+    //     answer: "Document Object Model",
+    // },
