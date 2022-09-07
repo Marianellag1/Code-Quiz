1f99973c6dc8454de1212c22e982aa4d54e6d515
Mgamboa <marianellag400@gmail.com>
2022-09-06 22:43:38 -0500

    tried connecting questions to button

--- README.md
+++ README.md
@@ -3,11 +3,13 @@ GIVEN I am taking a code quiz:
 
 WHEN I click the start button
 THEN a timer starts and I am presented with a question
-    //when the user clicks start timer should start counting down
-    //Questions from what I have learned, up to 5 questions
-
+    //when the user clicks start quiz and timer should start counting down
+    //Questions from what I have learned, up to 5 questions in total
+    //if else statement on getting answers right or wrong, which if wrong, deducts points ie seconds
+    //if answer is correct, time stays, next question should pop up
 WHEN I answer a question
 THEN I am presented with another question
+    //answers if right or wrong should show when user clicks on an answer
     //Questions will be made one after the other, all adding up whether right or wrong until final score at the end
         //My questions:
         // When data is stored in a single variable, it's called a(n): array  can choose: boolean-list-object
@@ -20,14 +22,11 @@ THEN time is subtracted from the clock
     //if-else statement has to be made.
     //if answer is wrong, subtraction of the timer will be taken
     //if anwser is correct timer continues as normal
-
-
 WHEN all questions are answered or the timer reaches 0
 THEN the game is over
     //If answers are wrong, the timer deducts, making the game quickly end
     //if questions aren't answered on time and time runs out, it ends
     //if all answers are answered right, it ends
-
 WHEN the game is over
 THEN I can save my initials and my score 
     user is shown score, as well as insert initials to collect score and keep record, for next game
--- jscript.js
+++ jscript.js
@@ -1,85 +1,67 @@
 var startButton = document.querySelector("#start-button");
-var codeQuiz = document.querySelector("#quiz");
 var scoreResult = document.querySelector("#score-results");
 var quizTimer = document.querySelector(".quiz-timer");
 var rightAnswer;
-var secondsLeft = 75;
+var secondsLeft = 50;
+var count = localStorage.getItem("count");
+
+var answerChoices = document.getElementById("answer-choices");
+var questionIndex = 0;
 
 startButton.addEventListener("click", function () {
-    // function setTimer() {
     var timerInterval = setInterval(function () {
         secondsLeft--;
-        quizTimer.textContent = "Time: " + secondsLeft;
-
+        quizTimer.textContent = "Time: " + secondsLeft + " second(s) left";
         if (secondsLeft == 0) {
             clearInterval(timerInterval);
         }
     }, 1000);
-    // function showResults(){ };
-    // quizBuild();
-    // startButton.addEventListener('click', showResults);
-    var quizQuest = [
-        {
-            title: "When data is stored in a single variable, it's called a(n):",
-            choices: ["Boolean", "List", "Object", "Array"],
-            answer: "Array",
-        },
-        {
-            title: "HTML gives structure to the page, CSS adds style to the page, while JavaScript gives the user... to the page.",
-            choices: ["Github", "Interaction", "Heading", "Class"],
-            answer: "Interaction",
-        },
-        {
-            title: "When writing code, all should end with...",
-            choices: ["?", "~", ";", "^"],
-            answer: ";",
-        },
-        {
-            title: "A variable declared in global scope is available to... function(s).",
-            choices: ["Some", "All", "None", "One"],
-            answer: "All",
-        },
 
-        {
-            title: "D.O.M. stands for...",
-            choices: ["Document Object Model", "Doughnuts on Monday", "Don't Open Meeting", " Drove On Mars"],
-            answer: "Document Object Model",
-        }
-    ]
 });
 
-function quizBuild() {
-    var output = [];
+ function startQuiz() {
+ var queryQuiz = quizQuest[i];
+//  questTitle.textContent = queryQuiz.text;
+ answerChoices.innerHTML = "";
 
-    for (var i = 0; i < quizQuest; i++) {
-        console.log(quizQuest);
-        var answers = [];
+    for (var i = 0; i < queryQuiz.length; i++) {
+    var choiceButton = document.createElement('button');
+
+    choiceButton.setAttribute("class", "btn");
+    choiceButton.textContent = queryQuiz.choices[i];
+    answerChoices.appendChild(choiceButton);
     }
-
 };
+var quizQuest = [
+    {
+        title: "When data is stored in a single variable, it's called a(n):",
+        choices: ["Boolean", "List", "Object", "Array"],
+        answer: "Array",
+    },
+    {
+        title: "HTML gives structure to the page, CSS adds style to the page, while JavaScript gives the user... to the page.",
+        choices: ["Github", "Interaction", "Heading", "Class"],
+        answer: "Interaction",
+    },
+    {
+        title: "When writing code, all should end with...",
+        choices: ["?", "~", ";", "^"],
+        answer: ";",
+    },
+    {
+        title: "A variable declared in global scope is available to... function(s).",
+        choices: ["Some", "All", "None", "One"],
+        answer: "All",
+    },
 
-function startGame() {
-    // isWin = false;
-    timerCount = 10;
-    // Prevents start button from being clicked when round is in progress
-    startButton.disabled = true;
-    // renderBlanks()
-    startTimer()
-}
-startButton.addEventListener("click", startGame);
+    {
+        title: "D.O.M. stands for...",
+        choices: ["Document Object Model", "Doughnuts on Monday", "Don't Open Meeting", " Drove On Mars"],
+        answer: "Document Object Model",
+    },
+]
+// quizQuest.addEventListener("click");
 
-//   function startTimer() {
-// Sets timer
-// quizTimer = setInterval(function() {
-//   timerCount--;
-//   timerElement.textContent = timerCount;
-//   if (timerCount >= 0) {
-// Tests if win condition is met
-// if (rightAnswer && timerCount > 0) {
-// Clears interval and stops timer
-//       clearInterval(timer);
-//       winGame();
-//     } 
-// };
-
-setTimer();
+// var questTitle = document.querySelector(".btn").addEventListener("click", function () {
+//     quizQuest("");
+// });
